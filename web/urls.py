e65d7f617cc70f31ab02f12dfc04d86c8ee39a9d
James Turk <dev@jamesturk.net>
2020-08-17 14:43:26 -0400

    remove some django stuff

--- web/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from api import endpoints
-
-urlpatterns = [
-    path("jurisdictions/", endpoints.JurisdictionEndpoint().as_django_view()),
-    path("people/", endpoints.PeopleEndpoint().as_django_view())
-]
