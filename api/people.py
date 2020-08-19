3a4a569068c07b29fc0a32225dd39604c5ca00b2
James Turk <dev@jamesturk.net>
2020-08-19 17:59:33 -0400

    other Person filtering

--- api/people.py
+++ api/people.py
@@ -1,9 +1,10 @@
 from typing import Optional, List
 from enum import Enum
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
+from sqlalchemy import func
 from openstates_metadata import lookup
 from .db import SessionLocal, get_db, models
-from .schemas import Person
+from .schemas import Person, OrgClassification
 from .pagination import Pagination
 from .auth import apikey_auth
 from .utils import joined_or_noload
@@ -36,27 +37,49 @@ def jurisdiction_filter(j):
     "/people", response_model=Pagination.of(Person), response_model_exclude_none=True
 )
 async def people(
-    jurisdiction: str,
-    chamber: Optional[str] = None,
+    jurisdiction: Optional[str] = None,
     name: Optional[str] = None,
+    id: Optional[List[str]] = Query([]),
+    org_classification: Optional[OrgClassification] = None,
     segments: List[PersonSegment] = Query([]),
     db: SessionLocal = Depends(get_db),
     pagination: Pagination = Depends(Pagination),
     auth: str = Depends(apikey_auth),
 ):
-    query = db.query(models.Person).order_by(models.Person.name)
+    query = (
+        db.query(models.Person).join(models.Jurisdiction).order_by(models.Person.name)
+    )
+    filtered = False
 
-    query = query.filter(
-        jurisdiction_filter(jurisdiction),
-        models.Person.current_role.isnot(None),  # current members only for now
-    ).join(models.Jurisdiction)
+    if jurisdiction:
+        query = query.filter(
+            jurisdiction_filter(jurisdiction),
+            models.Person.current_role.isnot(None),  # current members only for now
+        )
+        filtered = True
+    if name:
+        query = query.filter(func.lower(models.Person.name) == name.lower())
+        filtered = True
+    if id:
+        query = query.filter(models.Person.id.in_(id))
+        filtered = True
+    if org_classification:
+        query = query.filter(
+            models.Person.current_role["org_classification"].astext
+            == org_classification
+        )
+
+    if not filtered:
+        raise HTTPException(400, "either 'jurisdiction', 'name', or 'id' is required")
 
     # handle segments
     query = joined_or_noload(query, PersonSegment.other_names, segments)
     query = joined_or_noload(query, PersonSegment.other_identifiers, segments)
     query = joined_or_noload(query, PersonSegment.links, segments)
     query = joined_or_noload(query, PersonSegment.sources, segments)
-    query = joined_or_noload(query, PersonSegment.offices, segments, dbname="contact_details")
+    query = joined_or_noload(
+        query, PersonSegment.offices, segments, dbname="contact_details"
+    )
 
     resp = pagination.paginate(query)
 
