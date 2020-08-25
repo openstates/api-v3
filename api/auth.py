b102edd9756372a7ff32df095270f403c841df77
James Turk <dev@jamesturk.net>
2020-08-25 15:36:22 -0400

    rename X-APIKEY to X-API-KEY

--- api/auth.py
+++ api/auth.py
@@ -3,13 +3,13 @@ from fastapi import Header, HTTPException
 
 
 async def apikey_auth(
-    apikey: Optional[str] = None, x_apikey: Optional[str] = Header(None)
+    apikey: Optional[str] = None, x_api_key: Optional[str] = Header(None)
 ):
-    valid_apikey = x_apikey or apikey
+    valid_apikey = x_api_key or apikey
     if not valid_apikey:
         raise HTTPException(
             403,
-            detail="Must provide API Key as ?apikey or X-APIKEY. "
+            detail="Must provide API Key as ?apikey or X-API-KEY. "
             "Login and visit https://openstates.org/account/profile/ for your API key.",
         )
     yield valid_apikey
