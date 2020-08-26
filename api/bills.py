46a8601669276071b19d7715d02bbb86981f9580
James Turk <dev@jamesturk.net>
2020-08-26 17:04:58 -0400

    more bills filters

--- api/bills.py
+++ api/bills.py
@@ -1,9 +1,10 @@
 from typing import Optional, List
 from enum import Enum
-from fastapi import APIRouter, Depends, Query  # , HTTPException
+from fastapi import APIRouter, Depends, Query, HTTPException
 
 # from sqlalchemy import func, or_
 from sqlalchemy.orm import contains_eager
+from openstates_metadata import lookup
 from .db import SessionLocal, get_db, models
 from .schemas import Bill
 from .pagination import Pagination
@@ -19,12 +20,34 @@ class BillSegment(str, Enum):
 router = APIRouter()
 
 
+def jurisdiction_filter(j):
+    if len(j) == 2:
+        try:
+            return (
+                models.LegislativeSession.jurisdiction_id
+                == lookup(abbr=j).jurisdiction_id
+            )
+        except KeyError:
+            return models.Jurisdiction.name == j
+    elif j.startswith("ocd-jurisdiction"):
+        return models.LegislativeSession.jurisdiction_id == j
+    else:
+        return models.Jurisdiction.name == j
+
+
 @router.get(
     "/bills", response_model=Pagination.of(Bill), response_model_exclude_none=True
 )
 async def bills(
     jurisdiction: Optional[str] = None,
-    id: Optional[List[str]] = Query([]),
+    session: Optional[str] = None,
+    chamber: Optional[str] = None,
+    classification: Optional[str] = None,
+    subject: Optional[str] = None,
+    updated_since: Optional[str] = None,
+    action_since: Optional[str] = None,
+    # sponsor
+    # q
     segments: List[BillSegment] = Query([]),
     db: SessionLocal = Depends(get_db),
     pagination: Pagination = Depends(Pagination),
@@ -33,15 +56,33 @@ async def bills(
     query = (
         db.query(models.Bill)
         .join(models.Bill.legislative_session)
-        .order_by(models.Bill.identifier)
+        .join(models.Bill.from_organization)
+        .order_by(models.LegislativeSession.identifier, models.Bill.identifier)
         .options(contains_eager(models.Bill.legislative_session))
+        # .options(contains_eager(models.Bill.legislative_session.jurisdiction))
     )
-    # filtered = False
+    filtered = False
 
-    # if not filtered:
-    #     raise HTTPException(400, "either 'jurisdiction', 'name', or 'id' is required")
+    if jurisdiction:
+        query = query.filter(jurisdiction_filter(jurisdiction))
+        filtered = True
+    if session:
+        query = query.filter(models.LegislativeSession.identifier == session)
+    if chamber:
+        query = query.filter(models.Organization.classification == chamber)
+    if classification:
+        query = query.filter(models.Bill.classification.any(classification))
+    if subject:
+        query = query.filter(models.Bill.subject.any(subject))
+    if updated_since:
+        query = query.filter(models.Bill.updated_at > updated_since)
+    if action_since:
+        query = query.filter(models.Bill.latest_action_date > action_since)
 
-    # handle segments
+    if not filtered:
+        raise HTTPException(400, "either 'jurisdiction' or 'q' required")
+
+    # TODO: handle segments
     # query = joined_or_noload(query, PersonSegment.other_names, segments)
 
     resp = pagination.paginate(query)
