49c7e7da10c245c1b38374a3a203c78de6a39ec9
James Turk <dev@jamesturk.net>
2021-07-23 17:19:29 -0400

    committee list view

--- api/committees.py
+++ api/committees.py
@@ -5,6 +5,7 @@ from .db import SessionLocal, get_db, models
 from .schemas import Committee
 from .pagination import Pagination
 from .auth import apikey_auth
+from .utils import jurisdiction_filter
 
 
 router = APIRouter()
@@ -33,6 +34,37 @@ class CommitteePagination(Pagination):
 
 
 @router.get(
+    "/committees",
+    response_model=CommitteePagination.response_model(),
+    response_model_exclude_none=True,
+    tags=["committees"],
+)
+async def committee_list(
+    jurisdiction: str,
+    include: List[CommitteeInclude] = Query(
+        [], description="Additional includes for the Committee response."
+    ),
+    db: SessionLocal = Depends(get_db),
+    auth: str = Depends(apikey_auth),
+    pagination: CommitteePagination = Depends(),
+):
+    query = (
+        db.query(models.Organization)
+        .filter(
+            models.Organization.classification.in_(("committee", "subcommittee")),
+            jurisdiction_filter(jurisdiction, jid_field=models.Jurisdiction.id),
+        )
+        .order_by(models.Organization.name)
+    )
+
+    # handle parameters
+    # if classification:
+    #     query = query.filter(models.Jurisdiction.classification == classification)
+
+    return pagination.paginate(query, includes=include)
+
+
+@router.get(
     # we have to use the Starlette path type to allow slashes here
     "/committees/{committee_id:path}",
     response_model=Committee,
@@ -47,8 +79,7 @@ async def committee_detail(
     db: SessionLocal = Depends(get_db),
     auth: str = Depends(apikey_auth),
 ):
-    """ Get details on a single committee by ID. """
-    print(committee_id)
+    """Get details on a single committee by ID."""
     query = db.query(models.Organization).filter(
         models.Organization.id == committee_id,
         models.Organization.classification.in_(("committee", "subcommittee")),
