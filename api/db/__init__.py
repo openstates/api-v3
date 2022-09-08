caa928dba87a8c455ea3b8bf03cdd629afb3fc4b
John Seekins <john@civiceagle.com>
2022-09-08 08:41:20 -0600

    actually document current settings and set pool_recycle
    Signed-off-by: John Seekins <john@civiceagle.com>

--- api/db/__init__.py
+++ api/db/__init__.py
@@ -11,11 +11,13 @@ Default pool/overflow size is 5/10, timeout 30 seconds
 max_overflow=10 - the number of connections to allow in connection pool “overflow”,
     that is connections that can be opened above and beyond the pool_size setting,
     which defaults to five. this is only used with QueuePool.
+    Currently set to 15.
 
 pool_size=5 - the number of connections to keep open inside the connection pool.
     This used with QueuePool as well as SingletonThreadPool.
     With QueuePool, a pool_size setting of 0 indicates no limit; to disable pooling,
     set poolclass to NullPool instead.
+    Current set to 10 (double the default to give us more space as we grow)
 
 pool_timeout=30 - number of seconds to wait before giving up on getting a connection from the pool.
     This is only used with QueuePool. This can be a float but is subject to the limitations of
@@ -26,8 +28,9 @@ pool_recycle=-1 - this setting causes the pool to recycle connections after the 
     connections will be recycled after one hour. Note that MySQL in particular will disconnect
     automatically if no activity is detected on a connection for eight hours
     (although this is configurable with the MySQLDB connection itself and the server configuration as well).
+    Currently set to 28800 (8 hours) to match MySQL behavior.
 """
-engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=15, pool_timeout=30)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=15, pool_timeout=30, pool_recycle=28800)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
